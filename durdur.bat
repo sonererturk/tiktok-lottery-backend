cc1a700d408eb2cb730412b39581072cf2ad7741
Soner <37414103+sonererturk@users.noreply.github.com>
2025-06-07 16:28:19 +0300

    Delete durdur.bat

--- durdur.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title TikTok Cekilisci - Durdur
-echo TikTok Cekilisci sunucusu durduruluyor...
-echo.
-taskkill /F /IM node.exe
-echo.
-echo Sunucu durduruldu!
-echo Yeniden baslatmak icin 'baslat.bat' dosyasini calistirin.
-timeout /t 3 
